efb3cd9234d06212269390f060975ee56df02960
geeveet <geeveet@ifp-alguero.org>
2025-04-26 19:03:15 +0200

    DISSABTE WEB 01

--- js/script.js
+++ js/script.js
@@ -15,26 +15,15 @@ if (!searchContainer.contains(event.target)) {
 });
 
 
+// APARICION OOPS
 
-// ----- GEEVEET EN MAPA
-const givitImgs = document.querySelectorAll('.givit');
-const geeveetPopups = document.querySelectorAll('.geeveet_popup');
-
-if (givitImgs.length > 0 && geeveetPopups.length > 0) {
-    givitImgs.forEach(givit => {
-        givit.addEventListener('click', () => {
-            const id = givit.dataset.id;
-
-            // Toggle del activo
-            const yaActivo = givit.classList.contains('activa');
-            givitImgs.forEach(g => g.classList.remove('activa'));
-            geeveetPopups.forEach(p => p.classList.remove('visible'));
-
-            if (!yaActivo) {
-                givit.classList.add('activa');
-                const popupToShow = document.querySelector(`.geeveet_popup[data-id="${id}"]`);
-                if (popupToShow) popupToShow.classList.add('visible');
-            }
-        });
+document.addEventListener("DOMContentLoaded", function() {
+    const voluntariatBox = document.querySelector('.voluntariat_proper');
+    const activadores = document.querySelectorAll('.givit, .botonesmenu_inac');
+  
+    activadores.forEach(elem => {
+      elem.addEventListener('click', () => {
+        voluntariatBox.classList.add('mostrar');
+      });
     });
-}
+  });
